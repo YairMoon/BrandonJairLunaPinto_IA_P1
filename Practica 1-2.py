22c1b484c0555c9926804c7ea429c801f65bfe30
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-02-26 21:38:16 -0600

    "actualizacion de practicas 1"

---
+++ Practica 1-2.py
@@ -0,0 +1,12 @@
+print("hola mundo")
+print("holaaaaaa")
+print()#me puede generar un espacio en blanco
+print("mi nombre es:", "Jairo") #separar con comas
+print("Moon")#se imprime debajo de la anterior
+print("mi nombre es " , end = "" )#"#Jairo"se imprime en la misma linea
+print("Jairo")  
+print("Jair", "Luna", "Pinto", sep = "-")#lo separa con un guion
+print("Mi","Nombre","Es", sep = "_ ", end="*")#lo separa con un guion bajo y termina con un asterisco entre jairo y moon
+print("Jairo", "Moon", sep="*", end="*\n")
+print("2")#imprime el numero 2 como cadena
+print(2)#imprime el numero 2 como entero
