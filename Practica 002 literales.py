ef61cbcdb3b3ff9c83a35ff74ede66b530c2599f
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-03-01 20:44:33 -0600

    Literales en Python

---
+++ Practica 002 literales.py
@@ -0,0 +1,11 @@
+print(0o123)#octal
+print(0x123)#hexadecimal
+print(3e8)#notacion cientifica
+print(0.0000000000000000000001)
+print("Me gustan los \"changuitos\"")#para imprimir comillas dobles
+print('Me gustan los "changuitos"')#para imprimir comillas dobles de otra manera
+
+print(True > False)#boleanos true= 1
+print(True < False)#false =0
+
+print("\"holap\"\n\"\"soy\"\"\n\"\"\"Jair\"\"\"")
