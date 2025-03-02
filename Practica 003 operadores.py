417f2d6475790aef9b0a36c5e756ae64e423d604
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-03-01 21:08:56 -0600

    Operadores

---
+++ Practica 003 operadores.py
@@ -0,0 +1,16 @@
+print(5+9)
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+print(3 ** 2)#potencia
+print(6 // 4)#doble barra para division entera
+print(6. / 4)#una barra para division decimal
+print(69%6)#resto/residuo
+print(9 % 6 % 2)#residuo de residuo
+print((-3)**2)#potencia de negativo
+print(-2 ** 3)
+print(-(3 ** 2))
+print(3*5%4)#multiplicacion y residuo
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)#operaciones anidadas
+
