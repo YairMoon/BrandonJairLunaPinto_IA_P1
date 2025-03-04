d48c922cab32df53675ee85fe2195794e708d334
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-03-03 22:17:50 -0600

    variables

---
+++ Practica 004 variables.py
@@ -0,0 +1,17 @@
+changuito = 4
+print(changuito) #VARIABLES
+
+persona = "jairo"
+dinero = 569
+direccion = 'tabachines'
+print(persona, dinero, direccion) #IMPRIMIR VARIAS VARIABLES
+
+x = "Jairo Brandin"
+print("Me gustaria llamarme " + x) #COMBINAR UNA VARIABLE CON UNA CADENA
+
+name= "Jair Brandin " #AÑADIRLE UN NUEVO VALOR A UNA VARIABLE YA EXIXTENTE
+print(name)
+name=  name + "Luna"
+print(name)
+
+
