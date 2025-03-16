d467470669026526625d2ca581e7ab42e03af4bd
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-03-16 17:06:51 -0600

    se hacen varios cambios en el archivo de listas y se prueban 2 funciones

---
+++ Practica 007 listas.py
@@ -0,0 +1,17 @@
+# numeros = [8, 5, 3, 2, 1] #creamos una lista de numeros
+# print("Numeros originales:", numeros) #imprimimos la lista
+
+# numeros[1] = 800   #modificamos el segundo numero de la lista
+# print("Numeros modificados:", numeros) #imprimimos la lista modificada
+
+# print("\n tamaño de la lista:" , len(numeros)) #imprimimos el tamaño de la lista  len es una funcion que nos devulve el tamaño o numero de elementos de la lista
+
+# print(numeros) #imprimimos la lista
+
+# numeros = [8, 5, 3, 2, 1] #creamos una lista de numeros
+# del numeros[1] #eliminamos el segundo numero de la lista
+# print(len(numeros)) #imprimimos el tamaño de la lista
+# print(numeros) #imprimimos la lista
+
+numeros = [8, 5, 3, 2, 1] #creamos una lista de numeros
+print(numeros [-2]) #imprimimos el penultimo numero de la lista
