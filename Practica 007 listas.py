53dd5407b774d7768c991d1bd9940954673577c4
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-03-16 20:47:47 -0600

    modificacion de codigo

--- Practica 007 listas.py
+++ Practica 007 listas.py
@@ -17,8 +17,28 @@
 # print(numeros [-2]) #imprimimos el penultimo numero de la lista
 
 
-numeros = [8, 5, 3, 2, 1] #creamos una lista de numeros
-numeros[2]= int(input("Ingresa un numero para reemplazar al 3ro: ")) #reemplazamos el tercer numero de la lista
-del numeros[4] #eliminamos el ultimo numero de la lista
-print(len(numeros)) #imprimimos el tamaño de la lista
-print(numeros) #imprimimos la lista
+# numeros = [8, 5, 3, 2, 1] #creamos una lista de numeros
+# numeros[2]= int(input("Ingresa un numero para reemplazar al 3ro: ")) #reemplazamos el tercer numero de la lista
+# del numeros[4] #eliminamos el ultimo numero de la lista
+# print(len(numeros)) #imprimimos el tamaño de la lista
+# print(numeros) #imprimimos la lista
+
+# numeros = [8, 5, 3, 2, 1] #creamos una lista de numeros
+# print(len(numeros)) #imprimimos el tamaño de la lista
+# print(numeros) #imprimimos la lista
+
+
+# numeros .append(3)#esta es una funcion para insertar un numero en la lista y se ira al final
+# print(len(numeros))
+# print(numeros) #imprimimos la lista
+
+# numeros .insert(3, 100)#esta es una funcion para insertar un numero en la lista y sobre que posicion se va a insertar
+# print(len(numeros))
+# print(numeros) #imprimimos la lista
+
+
+# lista= [] #creamos una lista vacia
+# for i in range(5):
+#     #ista .append(i +1) # iniciara de 0 hasta 5 
+#     lista .insert(0,i+ 1) #lo hace al reves
+# print(lista)
